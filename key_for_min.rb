45b6e58af1c1d3ef6aadea0a2422e7abcc86d5bf
Claire Harmer <claire.f.harmer@gmail.com>
2018-07-22 22:50:04 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,6 @@
  def key_for_min_value(name_hash)
-  min_key = []
-  min_value = []
+  min_key = nil
+  min_value = nil
   name_hash.each do |key, value|
   if min_value == nil || value < min_value
   min_value = value
